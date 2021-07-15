d7ce887433ffa69c6052914a655ac12536fdd496
Yui-00 <19rd152@ms.dendai.ac.jp>
2021-07-15 21:55:01 +0900

    2

--- Main.java
+++ Main.java
@@ -1,8 +1,12 @@
 class Main {
   public static void main(String[] args) {
+    // 変更
     String msg = "" ;
     msg += "Hello " ;
     msg += "World!" ;
     System.out.println(msg) ;
+
+    int a = 5;
+    System.out.println(a) ;
   }
 }
